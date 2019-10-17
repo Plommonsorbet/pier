77b649b88362f84d8c9746cda1f7644699ff4a56
Isak Johansson <isak@k22.se>
2019-10-17 11:42:18 +0200

    Added things

--- tests/cli.rs
+++ tests/cli.rs
@@ -21,6 +21,7 @@ description = "This command creates a temporary file."
 command = "touch test.tmp"
 "#;
 
+
 pier_test!(cfg => CONFIG_OK_1, fn => test_list_scripts, | cfg: ChildPath, mut te: TestEnv | {
     // TODO Add some way to order the list output so it matches?
     te.cmd.arg("list");
@@ -38,6 +39,19 @@ command = "cat /etc/issue""#
     ));
 });
 
+pier_test!(cfg => 
+r#"
+[scripts.test_run]
+alias = "test_run"
+command = '''
+echo 'test'
+'''"#, fn => test_run_script_1, | cfg: ChildPath, mut te: TestEnv | {
+    te.cmd.args(&["run", "test_run"]);
+    te.cmd.assert().failure();
+    //te.cmd.assert().success();
+});
+
+
 //pier_test!(cfg => CONFIG_OK_1, fn => test_remove_script, | cfg: ChildPath, mut te: TestEnv | {
 //    te.cmd.args(&["remove", "touch-tmp"]);
 //    te.cmd.assert().success();
@@ -51,22 +65,9 @@ command = "cat /etc/issue""#
 //    ).trim());
 //});
 
-const CONFIG_OK_RUN: &'static str = r#"
-[scripts.test_1]
-alias = "test_1"
-command = '''echo 'test''''
-"#;
+//const CONFIG_OK_RUN: &'static str = r#"
+//[scripts.test_1]
+//alias = "test_1"
+//command = '''echo 'test''''
+//"#;
 
-pier_test!(cfg => 
-r#"
-[scripts.test_1]
-alias = "test_1"
-command = '''
-echo 'test'
-'''
-"#, 
-fn => test_run_script_1, | cfg: ChildPath, mut te: TestEnv | {
-    te.cmd.args(&["run", "test_1"]);
-    te.cmd.assert().failure();
-    //te.cmd.assert().success();
-});
